46fec79d01c85616560d862e154ba8e8226e4175
Anton Belski <anton.belski@gmail.com>
2023-12-05 19:12:53 +0300

    feat: add basic routing

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,40 @@
+import WelcomePage from '../pages/WelcomePage/WelcomePage';
+import LoginPage from '../pages/LoginPage/LoginPage';
+import MainPage from '../pages/MainPage/MainPage';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+import RegistrationPage from '../pages/RegistrationPage/RegistrationPage';
+import { Outlet, RouteObject, useRoutes } from 'react-router-dom';
+import { memo } from 'react';
+import { AllRoutes } from './allRoutes';
+
+const allRoutes: RouteObject = {
+  path: AllRoutes.root.path,
+  element: (
+    <>
+      <Outlet />
+    </>
+  ),
+  children: [
+    { index: true, element: <WelcomePage /> },
+    {
+      path: AllRoutes.login.path,
+      element: <LoginPage />,
+    },
+    {
+      path: AllRoutes.main.path,
+      element: <MainPage />,
+    },
+    {
+      path: AllRoutes.notFound.path,
+      element: <NotFoundPage />,
+    },
+    {
+      path: AllRoutes.registration.path,
+      element: <RegistrationPage />,
+    },
+  ],
+};
+
+export const AppRouter = memo(() => {
+  return useRoutes([allRoutes]);
+});
