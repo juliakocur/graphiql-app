f4220b6eb92ab4b1f7687a0a4b119268464340b0
mracoon <nataliyakomarova777@gmail.com>
2023-12-05 13:01:54 +0300

    chore: install mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Button from '@mui/material/Button';
 import './App.css';
 
 const App = () => {
-  return <h1>This is App</h1>;
+  return (
+    <>
+      <h1>This is App</h1>
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
 };
 
 export default App;
