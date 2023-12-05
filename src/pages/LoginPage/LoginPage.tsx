ba310d3dcf69022c7856919204f1ce91534d0b3d
mracoon <nataliyakomarova777@gmail.com>
2023-12-06 00:39:16 +0300

    fix: move redirect from main to login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import SignInForm from '../../components/Forms/SignInForm';
-import { logInWithEmailAndPassword } from '../../firebase/firebase';
+import { auth, logInWithEmailAndPassword } from '../../firebase/firebase';
 
 const LoginPage = () => {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user) navigate('/main', { replace: true });
+  }, [user, navigate]);
   return (
     <>
       <h1>SignIn</h1>
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,15 +1,6 @@
-import { useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
 import LogOutBtn from '../../components/Forms/LogOut';
-import { auth } from '../../firebase/firebase';
 
 const MainPage = () => {
-  const [user] = useAuthState(auth);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (user) navigate('/main', { replace: true });
-  }, [user, navigate]);
   return (
     <>
       <div>main</div>
