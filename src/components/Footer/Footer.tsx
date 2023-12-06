2c8ca66701873057691e2c8ffb87cf6ffb7c7bf9
Julia Kocur <juliakocur@mail.com>
2023-12-06 19:04:33 +0100

    feat: add footer content

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,31 @@
+import './footer.css';
+import rs_school from '/rs_school.svg';
+import git from '/git.png';
+
 const Footer = () => {
-  return <footer>FOOTER</footer>;
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="rss-logo">
+          <a href="https://rs.school/react/">
+            <img src={rs_school} alt="rs-school" />
+          </a>
+        </div>
+        <div className="year">2023</div>
+        <div className="git-accounts">
+          <a href="https://github.com/mracoon">
+            <img src={git} alt="git-account" />
+          </a>
+          <a href="https://github.com/Belski-Anton">
+            <img src={git} alt="git-account" />
+          </a>
+          <a href="https://github.com/juliakocur">
+            <img src={git} alt="git-account" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
