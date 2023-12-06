309d5d65491cc5e8d64fb6957af75b3ee3eb8fde
mracoon <nataliyakomarova777@gmail.com>
2023-12-06 07:19:12 +0300

    fix: fix redirection bug

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,19 +1,24 @@
 import { useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import SignInForm from '../../components/Forms/SignInForm';
-import { auth, logInWithEmailAndPassword } from '../../firebase/firebase';
+import { logInWithEmailAndPassword } from '../../firebase/firebase';
+import { useAppSelector } from '../../redux/reduxHooks';
 
 const LoginPage = () => {
-  const [user] = useAuthState(auth);
+  const { isTokenValid } = useAppSelector((state) => state.authReducer);
   const navigate = useNavigate();
   useEffect(() => {
-    if (user) navigate('/main', { replace: true });
-  }, [user, navigate]);
+    if (isTokenValid) navigate('/main', { replace: true });
+  }, [isTokenValid, navigate]);
+
   return (
     <>
-      <h1>SignIn</h1>
-      <SignInForm submitHandler={logInWithEmailAndPassword} />
+      {!isTokenValid && (
+        <>
+          <h1>SignIn</h1>
+          <SignInForm submitHandler={logInWithEmailAndPassword} />
+        </>
+      )}
     </>
   );
 };
