118b93f8fe1128529387c9daa4bec43f669c1c89
Julia Kocur <juliakocur@mail.com>
2023-12-08 19:25:27 +0100

    feat: add adaptive ut to 350px

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -15,7 +15,7 @@ const Editor = () => {
     key: string;
     value: string;
   }
-  const [data, setData] = useState<Idata[]>([{ key: '', value: '' }]);
+  const [data, setData] = useState<Idata[]>([]);
   const handleChangeKey = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
     index: number
@@ -43,90 +43,91 @@ const Editor = () => {
 
   return (
     <section className="graph">
-      <div className="url-input">
-        <div className="prettify-button">
-          <Button variant="contained" size="small" type="submit">
-            Prettify
-          </Button>
-        </div>
-        <input type="text" className="input-http"></input>
-      </div>
-      <div className="graph-container">
-        <div className="documentation"></div>
+      <div className="documentation">
         <button className="doc-button">
           <img src={folder} alt="documentation" />
         </button>
-        <div className="graph-content">
-          <form className="editor">
-            <div className="request-section">
+      </div>
+      <div className="graph-section">
+        <div className="url-input">
+          <div className="prettify-button">
+            <Button variant="contained" size="small" type="submit">
+              Prettify
+            </Button>
+          </div>
+          <input type="text" className="input-http"></input>
+        </div>
+        <div className="graph-container">
+          <div className="request-section">
+            <form className="editor">
               <div className="request">
                 <div className="request-response-title">Request</div>
                 <textarea name="query" className="request-area"></textarea>
               </div>
-              <div>
-                <div className="submit-button">
-                  <Button variant="contained" size="small" type="submit">
-                    ►
-                  </Button>
-                </div>
-              </div>
-            </div>
-
-            <div className="response">
-              <div className="request-response-title">Response</div>
-              <textarea name="query" className="response-area"></textarea>
-            </div>
-          </form>
-        </div>
-      </div>
-
-      <div className="variables-headers">
-        <div className="headers">
-          {data.map((item, index) => {
-            return (
-              <div className="header-item" key={index}>
-                <input type="checkbox" />
-                <input
-                  value={item.key}
-                  onChange={(e) => handleChangeKey(e, index)}
-                ></input>
-                <input
-                  value={item.value}
-                  onChange={(e) => handleChangeValue(e, index)}
-                ></input>
-                <div
-                  className="delete-button"
-                  onClick={() => handleDelete(index)}
-                >
-                  <Button variant="contained" size="small" type="submit">
-                    DEL
-                  </Button>
-                </div>
-              </div>
-            );
-          })}
-          <div className="add-button" onClick={handleClick}>
+              <div></div>
+            </form>
+          </div>
+          <div className="submit-button">
             <Button variant="contained" size="small" type="submit">
-              + Add new Header
+              ►
             </Button>
           </div>
+          <div className="response">
+            <div className="request-response-title">Response</div>
+            <textarea name="query" className="response-area"></textarea>
+          </div>
         </div>
-        <div className="variables">
-          <div>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography>Variables</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <TextField fullWidth required type="text">
-                  Text...
-                </TextField>
-              </AccordionDetails>
-            </Accordion>
+
+        <div className="variables-headers">
+          <div className="headers">
+            {data.map((item, index) => {
+              return (
+                <div className="header-item" key={index}>
+                  <input type="checkbox" />
+                  <input
+                    className="input-key"
+                    value={item.key}
+                    onChange={(e) => handleChangeKey(e, index)}
+                  ></input>
+                  <input
+                    className="input-value"
+                    value={item.value}
+                    onChange={(e) => handleChangeValue(e, index)}
+                  ></input>
+                  <div
+                    className="delete-button"
+                    onClick={() => handleDelete(index)}
+                  >
+                    <Button variant="contained" size="small" type="submit">
+                      DEL
+                    </Button>
+                  </div>
+                </div>
+              );
+            })}
+            <div className="add-button" onClick={handleClick}>
+              <Button variant="contained" size="small" type="submit">
+                + Add new Header
+              </Button>
+            </div>
+          </div>
+          <div className="variables">
+            <div>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>Variables</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <TextField fullWidth required type="text">
+                    Text...
+                  </TextField>
+                </AccordionDetails>
+              </Accordion>
+            </div>
           </div>
         </div>
       </div>
