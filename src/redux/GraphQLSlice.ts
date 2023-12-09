bacea58cec5817ab504b0a4310b632eed6c72715
mracoon <nataliyakomarova777@gmail.com>
2023-12-09 21:07:47 +0300

    feat: add graph to redux

---
+++ src/redux/GraphQLSlice.ts
@@ -0,0 +1,41 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+
+interface IGraphState {
+  url: string;
+  query: string;
+  headers: Record<string, string>;
+  variables: Record<string, string>;
+  response: string;
+}
+
+const initialState: IGraphState = {
+  url: '',
+  query: '',
+  headers: {},
+  variables: {},
+  response: '',
+};
+
+export const graphSlice = createSlice({
+  name: 'graph',
+  initialState,
+  reducers: {
+    setUrl(state, action: PayloadAction<string>) {
+      state.url = action.payload;
+    },
+    setQuery(state, action: PayloadAction<string>) {
+      state.query = action.payload;
+    },
+    setResponse(state, action: PayloadAction<string>) {
+      state.response = action.payload;
+    },
+    setVariables(state, action: PayloadAction<Record<string, string>>) {
+      state.variables = action.payload;
+    },
+    setHeaders(state, action: PayloadAction<Record<string, string>>) {
+      state.headers = action.payload;
+    },
+  },
+});
+
+export default graphSlice.reducer;
