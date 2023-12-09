842d2f72573c1c2b30f9e628fc9355fdb6a20550
mracoon <nataliyakomarova777@gmail.com>
2023-12-09 21:08:37 +0300

    feat: add query to redux

--- src/components/Editor/RequestSection.tsx
+++ src/components/Editor/RequestSection.tsx
@@ -1,9 +1,34 @@
+import { ChangeEvent, useState } from 'react';
+import { graphSlice } from '../../redux/GraphQLSlice';
+import { useAppDispatch, useAppSelector } from '../../redux/reduxHooks';
+
 export const RequestSection = () => {
+  const { setQuery } = graphSlice.actions;
+  const dispatch = useAppDispatch();
+  const { query } = useAppSelector((state) => state.graphReducer);
+
+  const [requestQuery, setRequestQuery] = useState('');
+
+  const handlerChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setRequestQuery(event.target.value);
+  };
+  const onBlurHandler = () => {
+    if (query !== requestQuery) {
+      dispatch(setQuery(requestQuery));
+    }
+  };
+
   return (
     <div className="editor request-section">
       <div className="request">
         <div className="request-response-title">Request</div>
-        <textarea name="query" className="request-area"></textarea>
+        <textarea
+          name="query"
+          className="request-area"
+          value={requestQuery}
+          onChange={handlerChange}
+          onBlur={onBlurHandler}
+        ></textarea>
       </div>
     </div>
   );
