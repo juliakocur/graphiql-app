4dc20cc99f6fcf85746707223b62da50664c6b8f
mracoon <nataliyakomarova777@gmail.com>
2023-12-09 21:14:32 +0300

    feat: add url to redux

---
+++ src/components/Editor/UrlInput.tsx
@@ -0,0 +1,29 @@
+import { useState, ChangeEvent } from 'react';
+import { graphSlice } from '../../redux/GraphQLSlice';
+import { useAppDispatch, useAppSelector } from '../../redux/reduxHooks';
+
+export const UrlInput = () => {
+  const { setUrl } = graphSlice.actions;
+  const dispatch = useAppDispatch();
+  const { url } = useAppSelector((state) => state.graphReducer);
+
+  const [urlValue, setUrlValue] = useState('');
+
+  const handlerChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUrlValue(event.target.value);
+  };
+  const onBlurHandler = () => {
+    if (url !== urlValue) {
+      dispatch(setUrl(urlValue));
+    }
+  };
+  return (
+    <input
+      type="text"
+      className="input-http"
+      value={urlValue}
+      onChange={handlerChange}
+      onBlur={onBlurHandler}
+    ></input>
+  );
+};
